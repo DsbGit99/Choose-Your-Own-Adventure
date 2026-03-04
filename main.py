9c7446651898a6416c96e9623b3114488c89d20d
Dan B <bristow.d444@gmail.com>
2026-03-03 19:27:09 -0500

    Setup FastAPI app with CORSMiddleware and uvicorn in main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI(
+    title="Choose Your Own Adventure Game API",
+    description="An API to generate stories for *Choose Your Own Adventure*.",
+    version="0.1.0",
+    docs_url="/docs",  # FastAPI comes with automatic documenation viewable from web broser
+    redoc_url="/redoc",
+)
+
+# Cross Origin Resource Sharing (CORS) is a security layer
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # What clients are allowed?
+    allow_credentials=True,  # i.e. SSL, TLS
+    allow_methods=["*"],  # i.e. GET, POST PUT, etc.
+    allow_headers=["*"],  # additional information included with requests
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    # NOTE: The "reload" parameter here enables auto-reloading during development,
+    # which automatically restarts the server when code changes are detected.
+
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
