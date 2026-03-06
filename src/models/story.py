f735a77c27e54a4b77ddec3e085538c8290436d4
Dan B <bristow.d444@gmail.com>
2026-03-05 20:15:16 -0500

    fixed typo in ForeignKey for story nodes

--- src/models/story.py
+++ src/models/story.py
@@ -21,7 +21,7 @@ class StoryNode(Base):
     __tablename__ = "story_nodes"
 
     id = Column(Integer, primary_key=True, index=True)
-    story_id = Column(Integer, ForeignKey="stories.id")
+    story_id = Column(Integer, ForeignKey("stories.id"))
     content = Column(String)
     is_root = Column(Boolean, default=False)
     is_ending = Column(Boolean, default=False)
